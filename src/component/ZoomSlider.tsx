fc773f7620651bd02a81a90d87c6f5e8e0886a67
Danny Lau <dannylau@Dannys-Macbook-Pro.local>
2020-02-18 23:44:37 +0800

    fix casing bug

---
+++ src/component/ZoomSlider.tsx
@@ -0,0 +1,68 @@
+import React from 'react';
+import Tooltip from '@material-ui/core/Tooltip';
+import IconButton from '@material-ui/core/Tooltip';
+import * as PIXI from 'pixi.js';
+import { Icon, Slider } from '@material-ui/core';
+import { mapViewer } from '..';
+import ZoomIn from '@material-ui/icons/ZoomIn';
+import ZoomOut from '@material-ui/icons/ZoomOut';
+
+interface ZoomSliderStates {
+	scale: number;
+}
+
+interface ZoomSliderProps {
+}
+
+const ValueLabelComponent = (props) => {
+	const { children, open, value } = props;
+	return (
+		<Tooltip open={open} enterTouchDelay={0} placement="top" title={value + "%"}>
+			{children}
+		</Tooltip>
+	);
+}
+
+export class ZoomSlider extends React.Component<ZoomSliderProps, ZoomSliderStates> {
+
+	constructor(props: ZoomSliderProps, context?: any) {
+		super(props, context);
+		this.state = {
+			scale: 100
+		};
+	}
+
+	zoom = (event, value) => {
+		this.setState({
+			scale: Math.max(10, Math.min(300, value))
+		});
+		mapViewer.stage.scale.set(this.state.scale / 100, this.state.scale / 100);
+	}
+
+	render() {
+		return (
+			<div className="slider">
+				<IconButton title='zoom' onClick={() => this.zoom(null, this.state.scale - 10)}>
+					<ZoomOut style={{ color: "hsl(0, 1%, 80%)" }} />
+				</IconButton>
+				<Slider
+					defaultValue={100}
+					value={this.state.scale}
+					aria-valuetext={"Zoom In / Out"}
+					aria-labelledby="discrete-slider"
+					valueLabelDisplay="auto"
+					ValueLabelComponent={ValueLabelComponent}
+					marks
+					track="inverted"
+					min={10}
+					max={300}
+					step={1}
+					onChange={this.zoom}
+				/>
+				<IconButton title='zoom' onClick={() => this.zoom(null, this.state.scale + 10)}>
+					<ZoomIn style={{ color: "hsl(0, 1%, 80%)" }} />
+				</IconButton>
+			</div >
+		);
+	}
+}
--- src/lib/MapPoint.ts
+++ src/lib/MapPoint.ts
@@ -16,14 +16,24 @@ export class MapPoint extends PIXI.Point {
     }
 
     public toMapSys(): MapPoint {
+        if (this.sys == PointSystem.MAP) return this;
         return new MapPoint(Math.floor(this.x / 32), Math.floor(this.x / 32), PointSystem.MAP);
     }
 
     public toPIXISys(): MapPoint {
+        if (this.sys == PointSystem.PIXI) return this;
         return new MapPoint(this.x * 32, this.y * 32);
     }
 
-    addXY(x: number, y: number): MapPoint {
+    addXY(x: number | MapPoint, y: number): MapPoint {
+        x instanceof MapPoint ? y = (x as MapPoint).y : y;
+        x instanceof MapPoint ? x = (x as MapPoint).x : x;
         return new MapPoint(this.x + x, this.y + y, this.sys);
+    }
+
+    distanceTo(x: number | MapPoint, y?: number): number {
+        x instanceof MapPoint ? y = (x as MapPoint).y : y;
+        x instanceof MapPoint ? x = (x as MapPoint).x : x;
+        return Math.sqrt(Math.pow(this.x - x, 2) + Math.pow(this.y - y, 2));
     }
 }
