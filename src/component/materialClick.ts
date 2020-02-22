05340f836f76d676d9111b633e8078575f3dc39c
Frank <40611willy@gmail.com>
2020-02-22 17:24:30 +0800

    Revert "Add classification tool"
    This reverts commit 15928a842a743ca6b4669cac486306ff66de12f9.

--- src/component/materialClick.ts
+++ src/component/materialClick.ts
@@ -1,15 +1,12 @@
-import { materialChoose } from "./classificationTool";
-
 let previousWhich = null;
 
 /**
  * @param {string} which  button ID
  */
-export const materialClick = (which: string, idx: number): void => {
+export const materialClick = (which: string): void => {
 	if (previousWhich != null)
 		document.getElementById(previousWhich).style.boxShadow = '';
 
 	document.getElementById(which).style.boxShadow = '0px 0px 5px 2.5px #ffffff';
 	previousWhich = which;
-	materialChoose(which, idx);
 }
