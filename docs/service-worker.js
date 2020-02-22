8da82813fdba899ef26a74c1823c6372f3f6e878
Frank <40611willy@gmail.com>
2020-02-22 22:09:39 +0800

    Revert "name change"
    This reverts commit 0fa7510530ce143f2f5c910eaea484916f6c0d1c.

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.6056bed624f799907d960ff0947237be.js"
+  "./precache-manifest.4022f3797ba93e2fe5a5552f2de6ca05.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
